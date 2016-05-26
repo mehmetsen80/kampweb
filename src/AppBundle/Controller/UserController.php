509a33f3d1360cb27c01877f9c8319a1751f5ff4
mburakergenc <muhammetergenc@gmail.com>
2016-05-26 15:17:15 -0500

    Update user info is completed

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -87,7 +87,7 @@ class UserController extends Controller
 //        ]);
 //    }
 
-        /**
+    /**
      * @Route("/update-info", name="update")
      */
     public function updateAction(Request $request){
